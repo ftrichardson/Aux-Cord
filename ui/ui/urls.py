09983889c16bfc23c77e6ba3336b04f7902acbf5
ftrichardson <flynnrichardson21@gmail.com>
2023-10-19 01:27:42 -0700

    Edit settings and urls

--- ui/ui/urls.py
+++ ui/ui/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('aux-cord.urls')),
+    *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
